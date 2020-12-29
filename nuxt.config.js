b7e907e6852120473b6a2314658f595dc4957e35
清水園子 <s-shimizu@tsumikiinc.com>
2020-12-30 03:40:42 +0900

    初期設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,10 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'mahjong',
+    htmlAttrs: {
+      lang: 'ja',
+    },
+    title: '麻雀',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -14,7 +17,23 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    {
+      src: '@/assets/css/main.scss',
+      lang: 'scss',
+    },
+  ],
+
+  styleResources: {
+    // 指定したSCSSファイルをvueファイル内のstyleタグの中で有効とする
+    scss: [
+      '@/assets/css/foundation/_variables.scss',
+      '@/assets/css/foundation/_functions.scss',
+      '@/assets/css/foundation/_keyframes.scss',
+      '@/assets/css/foundation/_mixin-utils.scss',
+      '@/assets/css/foundation/_variables-easings.scss',
+    ],
+  },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -26,6 +45,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -34,11 +54,18 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    'nuxt-webfontloader',
+    'nuxt-clipboard2',
   ],
+  webfontloader: {
+    google: {
+      families: ['Roboto:400,700'],
+    },
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-}
+};
