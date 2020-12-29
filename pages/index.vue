a9e85696b8000d41756c54b19326dbb48fe9161b
清水園子 <s-shimizu@tsumikiinc.com>
2020-12-30 04:42:18 +0900

    子がツモした場合の点数計算を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,58 @@
 <template lang="pug">
-  .container
-    h1.title
-      |麻雀
+.home
+  h1.home__title
+    |麻雀点数計算
+  .home__body
+    .home__item
+      |符：
+      input(
+        v-model="fu"
+        type="number"
+        )
+    .home__item
+      |上がりの翻数（ドラを含む）：
+      input(
+        v-model="fan"
+        type="number"
+        )
+    .home__total
+      h2.home__total-title 点数
+      .home__total-row
+        |自家子ツモ：子{{ ko }} / 親{{ oya }}
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      fu: 0,
+      fan: 0,
+    };
+  },
+  computed: {
+    base() {
+      const fu = Math.ceil(this.fu / 10) * 10;
+      return fu * Math.pow(2, this.fan) * 2 * 2;
+    },
+    oya() {
+      return Math.ceil((this.base * 2) / 100) * 100;
+    },
+    ko() {
+      return Math.ceil(this.base / 100) * 100;
+    },
+  },
+};
 </script>
 
-<style>
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+<style lang="scss">
+.home {
+  &__title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 }
 </style>
